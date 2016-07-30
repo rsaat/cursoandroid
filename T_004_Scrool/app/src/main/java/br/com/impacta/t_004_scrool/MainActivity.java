65ebf2b11f0603fe5c6a58c50c58749a3e79fda7
rsaat <rsaat@usa.com>
2016-07-30 18:50:35 -0300

    Correções erro de runtime no projeto

--- T_004_Scrool/app/src/main/java/br/com/impacta/t_004_scrool/MainActivity.java
+++ T_004_Scrool/app/src/main/java/br/com/impacta/t_004_scrool/MainActivity.java
@@ -16,8 +16,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.telainicial);
-        //
-        btn.setVisibility(View.GONE);
+        //btn.setVisibility(View.GONE);
     }
 
     @Override
